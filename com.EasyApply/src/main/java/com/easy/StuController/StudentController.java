555aaf3394f5a9ea45bbf274ee13e11436ab6ec1
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-20 19:35:18 +0530

    Refactor StudentController for cleaner code

--- com.EasyApply/src/main/java/com/easy/StuController/StudentController.java
+++ com.EasyApply/src/main/java/com/easy/StuController/StudentController.java
@@ -6,11 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.easy.Dto.StudentDTO;
-
 import com.easy.StuService.StudentService;
-
 import jakarta.validation.Valid;
 
 @RestController
@@ -27,12 +24,7 @@ public class StudentController {
 	 public ResponseEntity<String> register( @Valid @RequestBody StudentDTO student)
 	 
 	 {
-		
-		 service.register(student);
-		 
-		    return ResponseEntity.ok("User registered successfully");
-		     
-		     
+		 service.register(student);	 
+		    return ResponseEntity.ok("User registered successfully");     
 	 }
-
 }
